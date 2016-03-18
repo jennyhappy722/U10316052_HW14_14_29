aff38b9f18b3ce05c956c42d6cb2f8eee44591d0
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-18 21:50:19 +0800

    can show

---
+++ LineHW.java
@@ -0,0 +1,22 @@
+//u10316052
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Line;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Ellipse;
+//class
+public class LineHW extends Application{
+	@Override
+	public void start(Stage primaryStage){
+	
+	      Scene scene = new Scene(pane, 300, 300);
+    		primaryStage.setTitle("彈珠台"); // Set the stage title
+    		primaryStage.setScene(scene); // Place the scene in the stage
+    		primaryStage.show();	
+
+
+	}
+}
