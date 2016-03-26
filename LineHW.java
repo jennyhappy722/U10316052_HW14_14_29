01574ea4402785db27e2603f3740d1e2a60210a5
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-26 21:44:45 +0800

    Update LineHW.java

--- LineHW.java
+++ LineHW.java
@@ -67,7 +67,7 @@ public class LineHW extends Application{
 		//add to pane
 		pane.getChildren().addAll(line,line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16,e17,e18,e19,e20,e21,e22,e23,e24,e25,e26,e27,e28);
 		Scene scene = new Scene(pane, 300, 300);
-    		primaryStage.setTitle("彈珠台"); // Set the stage title
+    		primaryStage.setTitle("game"); // Set the stage title
     		primaryStage.setScene(scene); // Place the scene in the stage
     		primaryStage.show();	
 
